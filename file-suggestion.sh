68779306831b91a8335bd92401f257f8a79f839b
Michal Dziurowski <mdziurowski@gmail.com>
2026-01-24 18:54:27 +0100

    file suggestions with sort

--- file-suggestion.sh
+++ file-suggestion.sh
@@ -3,5 +3,5 @@ query=$(cat | jq -r '.query')
 
 # Match both files and directories
 fd --type f --type d --hidden 2>/dev/null | \
-  fzf --filter="$query" --no-sort | \
+  fzf --filter="$query" | \
   head -15
